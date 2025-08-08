d615c0ce1611df81a4c51df9db0c6a61b989fbbe
Jcrim <stellar.sea@outlook.com>
2025-08-08 22:07:09 +0800

    在线用户查询与修改用户名

--- user.go
+++ user.go
@@ -1,6 +1,9 @@
 package main
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 type User struct {
 	Name string
@@ -55,7 +58,46 @@ func (this *User) Offline() {
 
 // 用户处理消息的业务
 func (this *User) DoMessage(msg string) {
-	this.server.BroadCast(this, msg)
+	if msg == "who" {
+		// 查询当前在线用户都有哪些
+
+		this.server.mapLock.RLock()
+		for _, user := range this.server.OnlineMap {
+			onlineMsg := "[" + user.Addr + "]" + user.Name + ":" + "在线...\n"
+			this.SendMsg(onlineMsg)
+		}
+		this.server.mapLock.RUnlock()
+
+	} else if len(msg) > 7 && msg[:7] == "rename|" {
+		// 消息格式: rename|张三
+		// 安全地解析输入
+		parts := strings.SplitN(msg, "|", 2) // 最多切成2部分
+		if len(parts) != 2 || parts[1] == "" {
+			this.SendMsg("改名格式错误，请使用 'rename|新用户名' 格式，且新用户名不能为空。\n")
+			return // 提前返回，不再执行后续逻辑
+		}
+		newName := parts[1]
+
+		// 判断name是否存在
+		this.server.mapLock.Lock()
+		defer this.server.mapLock.Unlock()
+		_, ok := this.server.OnlineMap[newName]
+		if ok {
+			this.SendMsg("当前用户名被使用\n")
+		} else {
+			delete(this.server.OnlineMap, this.Name)
+			this.server.OnlineMap[newName] = this
+			this.Name = newName
+			this.SendMsg("您已经更新用户名:" + this.Name + "\n")
+		}
+	} else {
+		this.server.BroadCast(this, msg)
+	}
+}
+
+// 给当前 User 对应的客户端发送消息
+func (this *User) SendMsg(msg string) {
+	this.conn.Write([]byte(msg))
 }
 
 // 读主 routine 信息，发送给客户端
